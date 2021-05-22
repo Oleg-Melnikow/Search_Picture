62bb146f23a91dc2512b36249f92a63eee44948b
Oleg-Melnikow <melnikow.olezhka@mail.ru>
2021-05-22 13:14:11 +0300

    add settingsReducer

---
+++ src/redux/settingsReducer.ts
@@ -0,0 +1,26 @@
+export type RequestStatusType = 'idle' | 'succeeded';
+
+const initialState = {
+    status: 'idle' as RequestStatusType,
+    error: undefined as string | undefined
+}
+
+type InitialStateType = typeof initialState
+
+export const settingsReducer = (state: InitialStateType = initialState, action: ActionsType): InitialStateType => {
+    switch (action.type) {
+        case 'APP/SET-STATUS':
+            return {...state, status: action.status}
+        case 'APP/SET-ERROR':
+            return {...state, error: action.error}
+        default:
+            return state
+    }
+}
+
+export type setAppStatusActionType = ReturnType<typeof setAppStatusAC>
+export type setAppErrorActionType = ReturnType<typeof setAppErrorAC>
+export const setAppStatusAC = (status: RequestStatusType) => ({type: "APP/SET-STATUS", status} as const);
+export const setAppErrorAC = (error: string | undefined) => ({type: "APP/SET-ERROR", error} as const);
+
+type ActionsType = setAppStatusActionType | setAppErrorActionType
